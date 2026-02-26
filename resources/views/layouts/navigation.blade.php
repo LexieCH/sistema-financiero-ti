8cab7b9c3afd1b7ec556ac24dd3b7631593fe1df
Linda Guzman <linda.guzman@virginiogomez.cl>
2026-02-26 20:30:11 -0300

    ajustes css + navegacion movimientos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-    <!-- Primary Navigation Menu -->
+    <!-- Nav principal-->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
@@ -10,15 +10,22 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
+                <!-- Links navegación -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+
+                    <x-nav-link :href="route('dashboard')" 
+                    :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
-            </div>
 
-            <!-- Settings Dropdown -->
+                    <x-nav-link :href="route('movimientos.index')" 
+                    :active="request()->routeIs('movimientos.*')">
+                        {{ __('Movimientos') }}
+                    </x-nav-link>
+
+                </div>
+
+            <!-- menú desplegable -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -38,7 +45,7 @@
                             {{ __('Profile') }}
                         </x-dropdown-link>
 
-                        <!-- Authentication -->
+                        <!-- autentificación -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
@@ -52,7 +59,7 @@
                 </x-dropdown>
             </div>
 
-            <!-- Hamburger -->
+            <!-- abrir menú lateral-->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -64,15 +71,21 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
+    <!-- menu de nav adaptable-->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('dashboard')" 
+            :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('movimientos.index')" 
+            :active="request()->routeIs('movimientos.*')">
+                Movimientos
             </x-responsive-nav-link>
         </div>
 
-        <!-- Responsive Settings Options -->
+        <!-- opciones -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
@@ -84,7 +97,7 @@
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
+                <!-- autenticaci -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
